15bf9715caf1943775630bfebdc779cdc01e79ca
code with srnagur <acchinagur@gmail.com>
2024-07-20 14:12:52 +0530

    push work git

---
+++ workgit/src/test/java/contactgit/Contact.java
@@ -0,0 +1,11 @@
+package contactgit;
+
+public class Contact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("created succefully git");
+
+	}
+
+}
