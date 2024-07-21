75f3dd35dc8f7c03290146a79df41358f29c1715
abhilash <abhilash@thelibratech.com>
2024-07-21 19:14:18 +0530

    Updated TestScript For Contact and Organization

--- workgit/src/test/java/contactgit/Contact.java
+++ workgit/src/test/java/contactgit/Contact.java
@@ -3,8 +3,8 @@ package contactgit;
 public class Contact {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("created succefully git");
+		System.out.println("Contact created succefully");
+		System.out.println("Reshma successfully updated contact");
 
 	}
 
